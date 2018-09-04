3c96f53845b503ca79b77d2a30a4460adf544ff7
José manuel Barroso Galindo <theypsilon@gmail.com>
2018-09-05 01:59:29 +0200

    cosmetics

--- resources/shaders/frags.glsl
+++ resources/shaders/frags.glsl
@@ -3,14 +3,14 @@ out vec4 FragColor;
 
 in vec3 Normal;  
 in vec3 FragPos;  
-in vec4 VColor;
+in vec4 FragColor;
 
 uniform vec3 lightPos; 
 uniform vec3 lightColor;
 
 void main()
 {
-    if (VColor.a == 0.0) {
+    if (FragColor.a == 0.0) {
         discard;
     }
     
@@ -24,5 +24,5 @@ void main()
     float diff = max(dot(norm, lightDir), 0.0);
     vec3 diffuse = diff * lightColor;
             
-    FragColor = vec4(ambient + diffuse * 0.5, 1.0) * VColor;
+    FragColor = vec4(ambient + diffuse * 0.5, 1.0) * FragColor;
 } 
